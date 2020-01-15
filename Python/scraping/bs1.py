afb3bf185503e99415cafedf61c8cf7395b06d61
Viet Vo <viet.allen.qvo@gmail.com>
2020-01-15 11:51:45 -0800

    refactored bs1 to work on hackernews

--- Python/scraping/bs1.py
+++ Python/scraping/bs1.py
@@ -5,17 +5,24 @@ from bs4 import BeautifulSoup
 
 # Collect and parse first page
 page = requests.get(
-    'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+    'https://news.ycombinator.com/news'
+    # 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 )
 soup = BeautifulSoup(page.text, 'html.parser')
 
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
+# # Pull all text from the BodyText div
+# artist_name_list = soup.find(class_='BodyText')
+article_name_list = soup.find_all(class_='storylink')
 
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
+# # Pull text from all instances of <a> tag within BodyText div
+# artist_name_list_items = artist_name_list.find_all('a')
+# article_name_list_items = article_name_list.find('a')
 
-# Use .contents to pull out the <a> tag’s children
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    print(names)
+# # Use .contents to pull out the <a> tag’s children
+# for artist_name in artist_name_list_items:
+#     names = artist_name.contents[0]
+#     print(names)
+for  article_name in article_name_list:
+    article = article_name.contents[0]
+    print(article)
+# print(article_name_list_items)
