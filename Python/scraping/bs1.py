f6255c4a48c7337c75d271c8c3c79bf2bea0ca14
Viet Vo <viet.allen.qvo@gmail.com>
2020-01-16 09:21:48 -0800

    cleaned up bs1.py

--- Python/scraping/bs1.py
+++ Python/scraping/bs1.py
@@ -11,18 +11,9 @@ page = requests.get(
 soup = BeautifulSoup(page.text, 'html.parser')
 
 # # Pull all text from the BodyText div
-# artist_name_list = soup.find(class_='BodyText')
 article_name_list = soup.find_all(class_='storylink')
 
-# # Pull text from all instances of <a> tag within BodyText div
-# artist_name_list_items = artist_name_list.find_all('a')
-# article_name_list_items = article_name_list.find('a')
-
-# # Use .contents to pull out the <a> tag’s children
-# for artist_name in artist_name_list_items:
-#     names = artist_name.contents[0]
-#     print(names)
+# # Use .contents to pull out the tag’s children
 for  article_name in article_name_list:
     article = article_name.contents[0]
     print(article)
-# print(article_name_list_items)
